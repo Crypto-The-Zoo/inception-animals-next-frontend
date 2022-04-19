c7f322ac16aa240cc41445b12607df07ea9e80a3
alliu930410 <37877455+alliu930410@users.noreply.github.com>
2022-04-19 09:12:15 -0700

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,28 @@ import type { NextPage } from "next"
 import Navigation from "../components/Navigation"
 import Footer from "../components/Footer"
 import HomePage from "../components/Homepage"
+import Head from "next/head"
 
 const Home: NextPage = () => {
   return (
     <div className="w-full max-h-screen">
+      <div>
+        <Head>
+          <meta property="og:title" content="Inception Animals" key="title" />
+          <meta
+            property="og:description"
+            content="A retro futuristic metaverse brand"
+            key="description"
+          />
+          <meta property="og:url" content="https://www.inceptionanimals.com" />
+          <meta
+            property="og:image"
+            content={
+              "https://storage.googleapis.com/inception_public/cyber_hippo.jpg"
+            }
+          />
+        </Head>
+      </div>
       <Navigation />
       <HomePage />
       {/* <Footer /> */}
