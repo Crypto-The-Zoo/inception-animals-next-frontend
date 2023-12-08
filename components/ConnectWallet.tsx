0377922287528119ebd3c62152e9ec55fd8f4dec
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-12-08 17:24:52 -0500

    Add logout

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,12 +1,12 @@
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 // import Link from "next/link"
-import Link from "next/link"
-import { useContext } from "react"
-import { WalletContext } from "../context/WalletContext"
+import Link from "next/link";
+import { useContext } from "react";
+import { WalletContext } from "../context/WalletContext";
 // import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"
 
 const ConnectWalletNav: React.FC = () => {
-  const { walletAddr, connect, disconnect } = useContext(WalletContext)
+  const { walletAddr, connect, disconnect } = useContext(WalletContext);
 
   return (
     <section>
@@ -25,25 +25,35 @@ const ConnectWalletNav: React.FC = () => {
         )}
       </div>
     </section>
-  )
-}
+  );
+};
 
 const LoggedInMenu = (props: any) => {
-  const { walletAddr } = props
+  const { walletAddr, disconnect } = props;
   return (
     // TODO: add wallet view
     // <Link passHref href={`/users/${walletAddr}`}>
-    <Link passHref href={"my-inception-nfts"}>
-      <div className="text-inception-green font-inception-ink font-extrabold hover:text-inception-green transition-all duration-100 hover:bg-white px-4 py-2 bg-inception-off-white backdrop-blur-sm rounded bg-opacity-60 hover:cursor-pointer border-2 border-inception-green">
-        {/* {truncateAddr(walletAddr)} */}
-        Station
+    <>
+      <div className="flex gap-2">
+        <Link passHref href={"my-inception-nfts"}>
+          <div className="text-inception-green font-inception-ink font-extrabold hover:text-inception-green transition-all duration-100 hover:bg-white px-4 py-2 bg-inception-off-white backdrop-blur-sm rounded bg-opacity-60 hover:cursor-pointer border-2 border-inception-green">
+            {/* {truncateAddr(walletAddr)} */}
+            Station
+          </div>
+        </Link>
+        <button
+          className="text-inception-green font-inception-ink font-extrabold hover:text-inception-green transition-all duration-100 hover:bg-white px-4 py-2 bg-inception-off-white backdrop-blur-sm rounded bg-opacity-60 hover:cursor-pointer border-2 border-inception-green"
+          onClick={disconnect}
+        >
+          Log Out
+        </button>
       </div>
-    </Link>
-  )
-}
+    </>
+  );
+};
 
 const truncateAddr = (addr: string) => {
-  return addr.slice(0, 6) + "..." + addr.slice(-4)
-}
+  return addr.slice(0, 6) + "..." + addr.slice(-4);
+};
 
-export default ConnectWalletNav
+export default ConnectWalletNav;
