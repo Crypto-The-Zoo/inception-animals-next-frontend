c356f64b408c54aaac34db1926961f95385505bb
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-12-08 17:30:16 -0500

    Add logout user flow

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useContext } from "react";
 import { WalletContext } from "../context/WalletContext";
 // import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"
+import { useRouter } from "next/router";
 
 const ConnectWalletNav: React.FC = () => {
   const { walletAddr, connect, disconnect } = useContext(WalletContext);
@@ -30,6 +31,14 @@ const ConnectWalletNav: React.FC = () => {
 
 const LoggedInMenu = (props: any) => {
   const { walletAddr, disconnect } = props;
+
+  const router = useRouter();
+
+  const handleLogout = () => {
+    disconnect(); // Log out logic
+    router.push("/"); // Redirect to home page
+  };
+
   return (
     // TODO: add wallet view
     // <Link passHref href={`/users/${walletAddr}`}>
@@ -43,7 +52,7 @@ const LoggedInMenu = (props: any) => {
         </Link>
         <button
           className="text-inception-green font-inception-ink font-extrabold hover:text-inception-green transition-all duration-100 hover:bg-white px-4 py-2 bg-inception-off-white backdrop-blur-sm rounded bg-opacity-60 hover:cursor-pointer border-2 border-inception-green"
-          onClick={disconnect}
+          onClick={handleLogout}
         >
           Log Out
         </button>
