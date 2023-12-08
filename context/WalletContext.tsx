eac0f3affe5380ee9d9cc1f62570728609784c33
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-12-08 17:21:22 -0500

    Add multi-wallet

--- context/WalletContext.tsx
+++ context/WalletContext.tsx
@@ -1,53 +1,134 @@
 // @ts-ignore
-import { authenticate, unauthenticate, currentUser } from "@onflow/fcl"
-import { createContext, useCallback, useEffect, useState } from "react"
+import * as fcl from "@onflow/fcl";
+// @ts-ignore
+import { authenticate } from "@onflow/fcl";
+import { createContext, useCallback, useEffect, useState } from "react";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { init } from "@onflow/fcl-wc";
 
 interface ContextProps {
-  walletAddr: string | never
-  connect: () => void
-  disconnect: () => void
+  walletAddr: string | never;
+  connect: () => void;
+  disconnect: () => void;
 }
 
 export const WalletContext = createContext<ContextProps>({
   walletAddr: null!,
   connect: () => {},
   disconnect: () => {},
-})
+});
 
 interface DapperWallet {
-  addr: string
-  cid: string
-  expiresAt: string
-  f_type: string
-  f_vsn: string
-  loggedIn: boolean
-  services: string[]
+  addr: string;
+  cid: string;
+  expiresAt: string;
+  f_type: string;
+  f_vsn: string;
+  loggedIn: boolean;
+  services: string[];
 }
 
 const WalletProvider = ({ children }: any) => {
-  const [walletAddr, setWalletAddr] = useState<string | never>(null!)
+  const [walletAddr, setWalletAddr] = useState<string | never>(null!);
+  const [client, setClient] = useState(null);
 
-  const subscribeToAuthenticate = useCallback(() => {
-    currentUser().subscribe((wallet: DapperWallet) => {
-      setWalletAddr(wallet.addr)
-    })
-  }, [])
+  const wcSetup = useCallback(async (appTitle: string, iconUrl: string) => {
+    try {
+      const DEFAULT_APP_METADATA = {
+        name: appTitle,
+        description: appTitle,
+        url: window.location.origin,
+        icons: [iconUrl],
+      };
+
+      console.log({
+        DEFAULT_APP_METADATA,
+      });
+
+      const { FclWcServicePlugin, client } = await init({
+        projectId: "12ed93a2aae83134c4c8473ca97d9399", // required
+        metadata: DEFAULT_APP_METADATA, // optional
+        includeBaseWC: true, // optional, default: false
+      });
+
+      setClient(client);
+      fcl.pluginRegistry.add(FclWcServicePlugin);
+    } catch (e) {
+      throw e;
+    }
+  }, []);
 
   useEffect(() => {
-    subscribeToAuthenticate()
-  }, [])
+    const {
+      flowNetwork,
+      accessApi,
+      walletDiscovery,
+      walletDiscoveryApi,
+      walletDiscoveryInclude,
+      addresses,
+    } = {
+      flowNetwork: "mainnet",
+      accessApi: "https://rest-mainnet.onflow.org",
+      walletDiscovery: "https://fcl-discovery.onflow.org/authn",
+      walletDiscoveryApi: "https://fcl-discovery.onflow.org/api/authn",
+      walletDiscoveryInclude: [
+        "0xead892083b3e2c6c", // Dapper Wallet
+      ],
+      addresses: {
+        FlowToken: "0x1654653399040a61",
+        NonFungibleToken: "0x1d7e57aa55817448",
+        MetadataViews: "0x1d7e57aa55817448",
+        FungibleToken: "0xf233dcee88fe0abe",
+      },
+    };
+    const iconUrl = window.location.origin + "/images/wallet-icon.png";
+    const appTitle =
+      process.env.NEXT_PUBLIC_APP_NAME || "Inception Animals Connect";
+
+    console.log("--flowNetwork", flowNetwork);
+
+    fcl.config({
+      "app.detail.title": appTitle,
+      "app.detail.icon": iconUrl,
+      "accessNode.api": accessApi, // connect to Flow
+      "flow.network": flowNetwork,
+      "discovery.wallet": walletDiscovery, // use wallets on public discovery
+      "discovery.authn.endpoint": walletDiscoveryApi, // public discovery api endpoint
+      "discovery.authn.include": walletDiscoveryInclude, // opt-in wallets
+      "0xFungibleToken": addresses.FungibleToken,
+      "0xFlowToken": addresses.FlowToken,
+      "0xNonFungibleToken": addresses.NonFungibleToken,
+      "0xMetadataViews": addresses.MetadataViews,
+    });
+
+    if (!client) {
+      wcSetup(appTitle, iconUrl);
+    }
+  }, []);
+
+  const subscribeToAuthenticate = useCallback(() => {
+    fcl.currentUser().subscribe((wallet: DapperWallet) => {
+      setWalletAddr(wallet.addr);
+    });
+  }, []);
+
+  useEffect(() => {
+    subscribeToAuthenticate();
+  }, []);
 
   return (
     <WalletContext.Provider
       value={{
         walletAddr,
-        connect: authenticate,
-        disconnect: unauthenticate,
+        connect: fcl.authenticate,
+        disconnect: fcl.unauthenticate,
       }}
     >
       {children}
     </WalletContext.Provider>
-  )
-}
+  );
+};
 
-export default WalletProvider
+export default WalletProvider;
